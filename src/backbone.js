14bc91c95b064c2d191363c3e43d6c19159692fa
Bryan <bryanlohjw97@gmail.com>
2022-08-11 21:51:52 +0800

    build: backbone function
    backbone function to build sidebar

--- src/backbone.js
+++ src/backbone.js
@@ -1,22 +1,31 @@
 
+
 const backbone = () => {
     const content = document.querySelector('#content');
 
     const sidebar = document.createElement('div');
     sidebar.classList.add('sidebar');
 
+    const name = document.createElement('h1');
+    name.classList.add('restaurant-name');
+    name.innerHTML = 'Love Recipe';
+
     const home = document.createElement('div');
     home.classList.add('sidebtn');
-    home.setAttribute('id','side-home')
+    home.setAttribute('id','side-home');
+    home.innerHTML = 'Home';
 
     const about = document.createElement('div');
     about.classList.add('sidebtn');
-    about.setAttribute('id','side-about')
+    about.setAttribute('id','side-about');
+    about.innerHTML = 'About Us';
 
     const menu = document.createElement('div');
     menu.classList.add('sidebtn');
-    menu.setAttribute('id','side-menu')
+    menu.setAttribute('id','side-menu');
+    menu.innerHTML = 'Our Menu';
 
+    sidebar.appendChild(name);
     sidebar.appendChild(home);
     sidebar.appendChild(about);
     sidebar.appendChild(menu);
--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import './style.css';
 
 backbone();
 console.log('Hello Bryan');
+
+const body = document.querySelector('body');
